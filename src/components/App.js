aeba83adbf8b247e76ba76415a769e89691bcb40
prem-io <PremK.Lf@gmail.com>
2020-04-11 22:58:33 +0530

    add and reply tweet feat completed

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
 import Dashboard from './Dashboard'
+import NewTweet from './NewTweet'
 import LoadingBar from 'react-redux-loading'
+import TweetPage from './TweetPage'
 
 class App extends Component {
   componentDidMount() {
@@ -15,7 +17,7 @@ class App extends Component {
         <LoadingBar />
         {this.props.loading === true
           ? null
-          : <Dashboard />}
+          : <TweetPage match={{ params: { id: '8xf0y6ziyjabvozdd253nd' } }} />}
       </div>
     )
   }
